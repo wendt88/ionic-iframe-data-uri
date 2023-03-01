b27f5044b956c11ca476c0f2c659ece290f695ce
peter <peter.wendt@ilosgroup.com>
2023-03-01 16:19:25 +0100

    Initial commit

---
+++ src/components/ExploreContainer.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="container">
+    <strong>{{ name }}</strong>
+    <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    <iframe src="data:text/html;charset=utf-8,%3C!DOCTYPE%20html%3E%3Chtml%3E%3Chead%3E%3C/head%3E%3Cbody%3E%3Ch1%3EHi%3C/h1%3E%3C/body%3E%3C/html%3E"></iframe>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  name: String,
+});
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  color: #8c8c8c;
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+</style>
